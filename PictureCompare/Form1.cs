de5244d2d0cf45b00cebda386ecd9f044caeda87
darkertb <david911206@gmail.com>
2017-05-16 18:05:25 +0800

    first commit

---
+++ PictureCompare/Form1.cs
@@ -0,0 +1,53 @@
+﻿using PictureCompare.Script;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace PictureCompare {
+    public partial class Form1 : Form {
+        public Form1 () {
+            InitializeComponent();
+        }
+
+        public List<string> dupPic;
+
+        private void button1_Click (object sender, EventArgs e) {
+
+            string root = textBox1.Text != "" ? textBox1.Text : @"C:\Users\ChiaDe-Lin\Google 雲端硬碟\CV";
+            dupPic = PictureScanner.ins.Scan(root);
+            
+            listBox1.Items.Clear();
+            if (dupPic.Count > 0)
+                listBox1.Items.Add("全部");
+
+            for (int i = 0; i < dupPic.Count; i++) {
+                listBox1.Items.Add(dupPic[i]);
+            }
+        }
+
+        private void button2_Click (object sender, EventArgs e) {
+            bool isAll = false;
+
+            List<string> selectPaths = new List<string>();
+            for (int i = 0; i < listBox1.SelectedItems.Count; i++) {
+                string selectPath = listBox1.SelectedItems[i].ToString();
+                if (selectPath == "全部") {
+                    isAll = true;
+                    break;
+                }
+                selectPaths.Add(selectPath);
+            }
+
+            if (isAll) {
+                PictureScanner.ins.Remove(dupPic);
+                listBox1.Items.Clear();
+            }
+            else {
+                PictureScanner.ins.Remove(selectPaths);
+                for (int i = 0; i < listBox1.SelectedItems.Count; i++) {
+                    listBox1.Items.Remove(listBox1.SelectedItems[i]);
+                }
+            }
+        }
+    }
+}
---
+++ PictureCompare/Script/PictureScanner.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace PictureCompare.Script {
+    class PictureScanner {
+        public static PictureScanner ins = new PictureScanner();
+
+        public List<string> Scan (string root) {
+            Dictionary<string, string> picDict = new Dictionary<string, string>();
+            List<string> dupPicPath = new List<string>();
+            string[] picsPath = Directory.GetFiles(root, "*", SearchOption.AllDirectories);
+
+            for (int i = 0; i < picsPath.Length; i++) {
+                string picPath = picsPath[i];
+                string result = "";
+
+                using (StreamReader sr = new StreamReader(picPath)) {
+                    result = sr.ReadToEnd();
+                }
+                HashAlgorithm ha = HashAlgorithm.Create("MD5");
+
+                byte[] picByte = Encoding.Default.GetBytes(result);
+                byte[] hashByte = ha.ComputeHash(picByte);
+                
+                string hash = BitConverter.ToString(hashByte);
+
+                if (picDict.ContainsKey(hash))
+                    dupPicPath.Add(picPath);
+                else
+                    picDict.Add(hash, "");
+            }
+
+            return dupPicPath;
+        }
+
+        public int Remove (List<string> pathList) {
+            int count = 0;
+            for (int i = 0; i < pathList.Count; i++) {
+                File.Delete(pathList[i]);
+                count++;
+            }
+            return count;
+        }
+
+        public int Remove (string path) {
+            File.Delete(path);
+            return 1;
+        }
+    }
+}
